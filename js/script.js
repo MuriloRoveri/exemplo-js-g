509022e69db8f628ec9f10de029365d81445c045
MuriloRoveri <RM97893@fiap.com>
2023-03-07 11:49:09 -0300

    script.js adicionado ao html.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Este é um comentário de linha
+
+/**
+ * É um comentário
+ * de
+ * Bloco
+ */
+
+//Exemplo das funçôes 
+console.log("Olá Mundo!")
+console.error("Bom dia")
+console.warn("Sexo?")
+console.info("Vai ter...")
